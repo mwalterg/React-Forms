02b181604a13c8aee806f73b6efbee489ee070fc
Marvin Gill <marvg@Marvins-MBP.lan>
2023-07-27 17:01:34 -0400

    progress so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-
+import { useState } from "react";
 import './App.css'
+import Authenticate from "./components/Authenticate.jsx";
+import SignUpForm from "./components/SignUpForm.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <div>
-       
-      </div>
-      
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
-
-export default App
