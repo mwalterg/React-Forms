acdef5e75cac3a0deb925d320c19b0868dbdde63
mloisking <126358439+mloisking@users.noreply.github.com>
2023-07-28 13:07:07 -0400

    added codes to auauthenticate

--- src/components/signupform.jsx
+++ src/components/signupform.jsx
@@ -1,55 +1,55 @@
 import { useState } from 'react';
 
-
-
 export default function SignUpForm() {
-  const [username, setUsername] =useState ("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null)
-  async function handleSubmit() {
-    e.preventDefault();
-    try {
-      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-      body: JSON.stringify({
-            Username: "",
-            Password: "",
-          })
-        }
-      );
+  const [username, setUsername] =useState ("mloisking");
+  const [password, setPassword] = useState("Applebell44");
+  const [error, setError] = useState(null);
+async function handleSubmit() {
+  setToken(result.token);
+  e.preventDefault();
 
-      const result = await response.json();
-      console.log(result)
-    } catch (error) {
-      setError(error.message)
-    }
-    return (
-      <>
-        <h2>Sign Up</h2>
-        {error && <p>{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username:{" "}
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            Password:{" "}
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button>Submit</button>
-        </form>
-      </>
+  try {
+    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    body: JSON.stringify({
+          Username: "mloisking",
+          Password: "AppleBell44",
+        })
+      }
     );
+
+    const result = await response.json();
+    console.log(result)
+  } catch (error) {
+    setError(error.message)
+  }
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
   }
 }
